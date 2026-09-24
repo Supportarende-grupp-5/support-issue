1806c690637f104132867d2d055dfde88df85a4b
devran <devranaydin1777@gmail.com>
2026-09-24 17:06:37 +0200

    Add request model for ticket registration

---
+++ SupportIssue.Application/Models/CreateTicketRequest.cs
@@ -0,0 +1,12 @@
+﻿using SupportIssue.Domain.Enums;
+
+namespace SupportIssue.Application.Models;
+
+public class CreateTicketRequest
+{
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; } 
+    public Guid CustomerId { get; set; }
+    public TicketPriority Priority { get; set; } = TicketPriority.Normal;
+
+}
--- SupportIssue.Presentation/App.xaml.cs
+++ SupportIssue.Presentation/App.xaml.cs
@@ -1,14 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿namespace SupportIssue.Presentation;
 
-namespace SupportIssue.Presentation
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
-
 }
