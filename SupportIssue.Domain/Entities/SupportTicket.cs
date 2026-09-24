482e28b60500b9f56a32dc3702d74d6d8205a6ef
devran <devranaydin1777@gmail.com>
2026-09-24 16:25:32 +0200

    Add Classes and Construtctor

---
+++ SupportIssue.Domain/Entities/SupportTicket.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using SupportIssue.Domain.Enums;
+
+namespace SupportIssue.Domain.Entities;
+
+public class SupportTicket
+{
+    public Guid Id { get; private set; }
+    public Guid CustomerId { get; private set; }
+
+    public string? Title { get; private set; }
+    public string? Description { get; private set; }
+
+    public TicketPriority Priority { get; private set; }
+    public TicketStatus Status { get; private set; }
+
+    public DateTimeOffset CreatedAt { get; private set; }
+
+    public SupportTicket(
+
+        string title,
+        string description,
+        Guid CustomerId,
+        TicketPriority ticketPriority)
+    {
+
+    }
+  
+        
+
+
+
+
+
+
+
+
+
+
+
+}
