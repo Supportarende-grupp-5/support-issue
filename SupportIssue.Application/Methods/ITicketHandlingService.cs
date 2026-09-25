8380f6d77b75207f086b0d05c75db95ecaa6cabc
Gustav Andreasson <Gustav.Andreasson@ekebacken.se>
2026-09-25 09:38:19 +0200

    Adjust interface and implement it

--- SupportIssue.Application/Methods/ITicketHandlingService.cs
+++ SupportIssue.Application/Methods/ITicketHandlingService.cs
@@ -1,14 +1,17 @@
 ﻿
+using SupportIssue.Domain;
+using static SupportIssue.Domain.Ticket;
+
 namespace SupportIssue.Application.Methods;
 
 public interface ITicketHandlingService
 {
     public Task<Ticket> GetTicketById(Guid ticketId);
-    public Task<Ticket> EditTicket(Guid technicianId, string ticketTitle, string ticketDescription);
+    public Task<Ticket> EditTicket(Guid ticketId, string ticketTitle, string ticketDescription);
     public Task<Ticket> AssignTechnician(Guid ticketId, Guid technicianId);
-    public Task<Ticket> ChangeTicketStatus(Guid ticketId, string newStatus);
+    public Task<Ticket> ChangeTicketStatus(Guid ticketId, TicketStatus newStatus);
     public Task<Ticket> AddCommentToTicket(Guid ticketId, string comment);
-    public Task<Ticket> ChangePriority(Guid ticketId, string newPriority);
-    public Task<IEnumerable<Comment>> GetCommentsByTicketId(Guid ticketId);
+    public Task<Ticket> ChangePriority(Guid ticketId, TicketPriority newPriority);
+    public Task<IEnumerable<TicketComment>> GetCommentsByTicketId(Guid ticketId);
 
 }
--- SupportIssue.Application/Methods/TicketHandlingService.cs
+++ SupportIssue.Application/Methods/TicketHandlingService.cs
@@ -1,7 +1,39 @@
-﻿namespace SupportIssue.Application.Methods;
+﻿using SupportIssue.Domain;
+
+namespace SupportIssue.Application.Methods;
 
 public class TicketHandlingService : ITicketHandlingService
 {
+    public Task<Ticket> AddCommentToTicket(Guid ticketId, string comment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Ticket> AssignTechnician(Guid ticketId, Guid technicianId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Ticket> ChangePriority(Guid ticketId, string newPriority)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Ticket> ChangeTicketStatus(Guid ticketId, string newStatus)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Ticket> EditTicket(Guid technicianId, string ticketTitle, string ticketDescription)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<TicketComment>> GetCommentsByTicketId(Guid ticketId)
+    {
+        throw new NotImplementedException();
+    }
+
     public async Task<Ticket> GetTicketById(Guid ticketId)
     {
         try
