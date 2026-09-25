9cbbfeefd46aae93735451fe7a29db1131df90ed
devran <devranaydin1777@gmail.com>
2026-09-25 09:19:15 +0200

    Add ticket service interface

---
+++ SupportIssue.Application/Interfaces/ITicketService.cs
@@ -0,0 +1,10 @@
+﻿using SupportIssue.Application.Models;
+using SupportIssue.Domain.Entities;
+
+namespace SupportIssue.Application.Interfaces;
+
+public interface ITicketService
+{
+    SupportTicket CreateTicket(CreateTicketRequest request);
+
+}
