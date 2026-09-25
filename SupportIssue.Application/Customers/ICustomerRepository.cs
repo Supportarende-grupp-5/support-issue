70de9e93e7814d20a375c31cc2c20ecade6544bd
fredrik jorman <fredrik.jorman@gmail.com>
2026-09-25 09:22:24 +0200

    Add Customer model and repository interface.

---
+++ SupportIssue.Application/Customers/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using SupportIssue.Domain.Customers;
+namespace SupportIssue.Application.Customers;
+
+public interface ICustomerRepository
+{
+    void Add(Customer customer);
+    IReadOnlyList<Customer> GetAll();
+    Customer? GetById(Guid id);
+    void Update(Customer customer);
+
+}
---
+++ SupportIssue.Domain/Customers/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace SupportIssue.Domain.Customers;
+
+public class Customer
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
