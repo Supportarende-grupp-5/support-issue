7bbfbc9fcb69b0279648ad82496c73d35b16cca2
Gustav Andreasson <Gustav.Andreasson@ekebacken.se>
2026-09-25 09:17:34 +0200

    Add Interface for TicketHandling

---
+++ SupportIssue.Application/Methods/ITicketHandlingService.cs
@@ -0,0 +1,14 @@
+﻿
+namespace SupportIssue.Application.Methods;
+
+public interface ITicketHandlingService
+{
+    public Task<Ticket> GetTicketById(Guid ticketId);
+    public Task<Ticket> EditTicket(Guid technicianId, string ticketTitle, string ticketDescription);
+    public Task<Ticket> AssignTechnician(Guid ticketId, Guid technicianId);
+    public Task<Ticket> ChangeTicketStatus(Guid ticketId, string newStatus);
+    public Task<Ticket> AddCommentToTicket(Guid ticketId, string comment);
+    public Task<Ticket> ChangePriority(Guid ticketId, string newPriority);
+    public Task<IEnumerable<Comment>> GetCommentsByTicketId(Guid ticketId);
+
+}
---
+++ SupportIssue.Application/Methods/TicketHandlingService.cs
@@ -0,0 +1,17 @@
+﻿namespace SupportIssue.Application.Methods;
+
+public class TicketHandlingService : ITicketHandlingService
+{
+    public async Task<Ticket> GetTicketById(Guid ticketId)
+    {
+        try
+        {
+            await GetTicketById(ticketId);
+            return Ticket;
+        }
+        catch (Exception ex)
+        {
+            throw new ApplicationException("An error occurred while retrieving the ticket.", ex);
+        }
+    }
+}
