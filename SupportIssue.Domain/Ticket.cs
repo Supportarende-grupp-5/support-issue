f4c7445a3158e97c3faf2a91225c82b3e850dc5b
Gustav Andreasson <Gustav.Andreasson@ekebacken.se>
2026-09-25 09:37:55 +0200

    Added accepted string values for status/Priority

---
+++ SupportIssue.Domain/Ticket.cs
@@ -0,0 +1,21 @@
+﻿namespace SupportIssue.Domain;
+
+public class Ticket
+{
+    public Guid Id { get; private set; }
+    public string TicketTitle { get; set; } =string.Empty;
+    public Guid CustomerId { get; set; }
+    public string TicketDescription { get; set; } = string.Empty;
+    public enum TicketStatus
+    {
+        New,
+        InProgress,
+        Resolved
+    }
+    public enum TicketPriority
+    {
+        Low,
+        Medium,
+        High
+    }
+}
