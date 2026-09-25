bf4df5fb8fb8a7485cfd5be9daddf5c37fb6d95b
Gustav Andreasson <Gustav.Andreasson@ekebacken.se>
2026-09-25 09:19:29 +0200

    Add TicketCommentModel

---
+++ SupportIssue.Domain/TicketComment.cs
@@ -0,0 +1,17 @@
+﻿namespace SupportIssue.Domain;
+
+public class TicketComment
+{
+    public Guid Id { get; private set; }
+    public Guid TicketId { get; set; }
+    public string Comment { get; set; }
+    public DateTime CreatedAt { get; set; }
+    
+    public TicketComment(Guid ticketId, string comment)
+    {
+        Id = Guid.NewGuid();
+        TicketId = ticketId;
+        Comment = comment;
+        CreatedAt = DateTime.UtcNow;
+    }
+}
