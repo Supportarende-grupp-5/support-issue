35f59b828401524e4a8bf6f31dd3b7b1724c136a
Gustav Andreasson <Gustav.Andreasson@ekebacken.se>
2026-09-25 10:15:57 +0200

    Add repository for ticket handling

---
+++ SupportIssue.Infrastructure/ITicketHandlingRepository.cs
@@ -0,0 +1,9 @@
+﻿using SupportIssue.Domain;
+
+namespace SupportIssue.Infrastructure;
+
+public interface ITicketHandlingRepository
+{
+    Task<Ticket> GetTicketByIdAsync(Guid ticketId);
+    Task<IEnumerable<TicketComment>> GetCommentsByTicketIdAsync(Guid ticketId);
+}
---
+++ SupportIssue.Infrastructure/JsonFileTicketHandlingRepository.cs
@@ -0,0 +1,41 @@
+﻿using SupportIssue.Domain;
+using System.Text.Json;
+
+namespace SupportIssue.Infrastructure;
+
+public class JsonFileTicketHandlingRepository : ITicketHandlingRepository
+{
+    public async Task<Ticket> GetTicketByIdAsync(Guid ticketId)
+    {
+        {
+            if (!File.Exists(_filePath))
+            {
+                throw new FileNotFoundException("The ticket file was not found.", _filePath);
+            }
+            var json = await File.ReadAllTextAsync(_filePath);
+            var tickets = JsonSerializer.Deserialize<List<Ticket>>(json);
+            var ticket = tickets?.FirstOrDefault(t => t.Id == ticketId);
+            if (ticket == null)
+            {
+                throw new KeyNotFoundException($"Ticket with ID {ticketId} was not found.");
+            }
+            return ticket;
+        }
+    }
+    public async Task<IEnumerable<TicketComment>> GetCommentsByTicketIdAsync(Guid ticketId)
+    {
+        if (!File.Exists(_filePath))
+        {
+            throw new FileNotFoundException("The ticket comment file was not found.", _filePath);
+        }
+        var json = await File.ReadAllTextAsync(_filePath);
+        var Allcomments = JsonSerializer.Deserialize<List<TicketComment>>(json);
+        if (Allcomments == null)
+        {
+            throw new KeyNotFoundException($"No comments found");
+        }
+        var comments = Allcomments.Where(c => c.TicketId == ticketId).ToList();
+
+        return comments;
+    }
+}
